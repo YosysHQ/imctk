0e9b6170eb412e24f217c11db0f0828f8fb84472
Jannis Harder <me@jix.one>
2024-06-29 16:23:41 +0200

    transparent: Fix broken `NewtypeCast` derive macro

--- derive/src/transparent.rs
+++ derive/src/transparent.rs
@@ -1,18 +1,10 @@
-use proc_macro2::{Span, TokenStream};
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Ident};
+use syn::DeriveInput;
 
 use crate::{require_repr_transparent, resolve_crate, ReprTransparent};
 
 pub fn derive_subtype_cast(input: DeriveInput) -> syn::Result<TokenStream> {
-    derive_cast(input, Ident::new("SubtypeCast", Span::call_site()))
-}
-
-pub fn derive_newtype_cast(input: DeriveInput) -> syn::Result<TokenStream> {
-    derive_cast(input, Ident::new("NewtypeCast", Span::call_site()))
-}
-
-fn derive_cast(input: DeriveInput, trait_ident: Ident) -> syn::Result<TokenStream> {
     #![allow(non_snake_case)]
 
     let DeriveInput {
@@ -27,7 +19,7 @@ fn derive_cast(input: DeriveInput, trait_ident: Ident) -> syn::Result<TokenStrea
         field,
         field_ty: inner,
         extra_init,
-    } = require_repr_transparent(&ident, &trait_ident.to_string(), &attrs, &data)?;
+    } = require_repr_transparent(&ident, "SubtypeCast", &attrs, &data)?;
 
     let imctk_transparent = resolve_crate("imctk-transparent");
 
@@ -45,6 +37,31 @@ fn derive_cast(input: DeriveInput, trait_ident: Ident) -> syn::Result<TokenStrea
             const __STATIC_ASSERT_HELPER: () = {
                 let _ = |&Self { #field: _ #extra_init }: &Self| ();
             };
+        }
+    })
+}
+
+pub fn derive_newtype_cast(input: DeriveInput) -> syn::Result<TokenStream> {
+    #![allow(non_snake_case)]
+
+    let DeriveInput {
+        ident, generics, ..
+    } = input;
+
+    let imctk_transparent = resolve_crate("imctk-transparent");
+
+    let NewtypeCast = quote![#imctk_transparent::NewtypeCast];
+
+    let target_ident = ident;
+    let (impl_generics, type_generics, where_clause) = generics.split_for_impl();
+
+    let target_type = quote![#target_ident #type_generics];
+
+    Ok(quote! {
+        // SAFETY: this trait is unsafe to allow SubtypeCast types to uphold extra invariants, but
+        // since the derive macro is restricted to the definition site of the type it can only be
+        // used at the place where such a decision would be made
+        unsafe impl #impl_generics #NewtypeCast for #target_type #where_clause {
         }
     })
 }
