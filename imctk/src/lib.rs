82462119e61b2a79bb954e7d09f1cedfa8a38a80
Jannis Harder <me@jix.one>
2024-08-27 14:26:08 +0200

    Add empty placeholder crate for future public API

---
+++ imctk/src/lib.rs
@@ -0,0 +1,4 @@
+//! Incremental Model Checking Toolkit
+//!
+//! Currently IMCTK does not offer a public API. This crate is a placeholder and will be updated to
+//! contain the main entry point of IMCTK's public API.
