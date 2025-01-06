07822e60f65cebc10249919e16aa3fd6c7b379bd
Jannis Harder <me@jix.one>
2025-01-06 18:09:57 +0100

    wip

---
+++ docs/source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Incremental Model Checking Toolkit'
+copyright = '2024, YosysHQ GmbH'
+author = 'YosysHQ GmbH'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo-ys'
+html_static_path = ['_static']
+
+
+extensions.append('sphinx.ext.todo')
+todo_include_todos = True
---
+++ docs/source/index.rst
@@ -0,0 +1,9 @@
+
+Incremental Model Checking Toolkit documentation
+================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   formal_system
